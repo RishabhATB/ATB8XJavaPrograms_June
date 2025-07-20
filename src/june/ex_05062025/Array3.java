8837a461567dd2d6545c79a9a04186f595aab72f
Rishabh <mishrar.17@gmail.com>
2025-07-20 21:50:03 +0530

    Arrays, exceptions

---
+++ src/june/ex_05062025/Array3.java
@@ -0,0 +1,14 @@
+package june.ex_05062025;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 5; i > n; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
